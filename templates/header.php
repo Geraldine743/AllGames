f50b762763a8d4bd40bb2ab47e339bce4e6605b4
Geraldine743 <egeraldine2@gmail.com>
2025-05-23 15:00:49 +0200

    add structure page jeux

--- templates/header.php
+++ templates/header.php
@@ -15,7 +15,7 @@
                 <img src="assets/images/logo-all-games.svg" width="120" alt="logo allgames">
             </a>
             <nav class="md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center">
-                <a class="mr-5 hover:text-white">Liste des jeux</a>
+                <a href="jeux.php" class="mr-5 hover:text-white">Liste des jeux</a>
                 <a class="mr-5 hover:text-white">Ma liste</a>
             </nav>
             <button class="inline-flex items-center bg-gray-800 border-0 py-1 px-3 focus:outline-none hover:bg-gray-700 rounded text-base mt-4 md:mt-0">Connexion
