fdd653b8953bf6831343b22374b2c382b60a8e47
Geraldine743 <egeraldine2@gmail.com>
2025-05-23 14:57:25 +0200

    add function getAllGames()

--- index.php
+++ index.php
@@ -1,11 +1,9 @@
 <?php
 require_once "templates/header.php";
+require_once "libs/game.php";
 
-$games = [
-    ["name" => "Spider-Man 2", "description" => "Incarnez le célèbre super-héros dans une nouvelle aventure pleine d'action. Balancez-vous à travers la ville..."],
-    ["name" => "Like a Dragon: Infinite Wealth", "description" => "Explorez un monde ouvert rempli de possibilités et de défis. Incarnez un héros et affrontez..."],
-    ["name" => "Starfield", "description" => "Partez à la découverte des étoiles dans ce jeu de rôle et d'action futuriste. Explorez des planètes inconnues, interagissez avec des factions spatiales..."],
-]
+
+$games = getAllGames();
 
 ?>
 
